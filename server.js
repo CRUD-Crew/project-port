ee37d389a6f18c9258a1c84fdefbd662ff479058
brianschnee <brian.a.schnee@gmail.com>
2022-06-18 19:41:01 -0400

    beginning stages of adding MongoDB

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const { MongoClient } = require('mongodb');
 
 require('dotenv').config();
 const PORT = process.env.PORT || 8000;
 
 app.use(cors());
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.json({
