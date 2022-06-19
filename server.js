b090b11579ef624e0bf3881bebcfa91ff76a1aca
brianschnee <brian.a.schnee@gmail.com>
2022-06-18 21:13:28 -0400

    Connected to Mongo Atlas

--- server.js
+++ server.js
@@ -1,18 +1,27 @@
 const express = require('express');
 const app = express();
-const cors = require('cors');
 const { MongoClient } = require('mongodb');
+const cors = require('cors');
 
 require('dotenv').config();
 const PORT = process.env.PORT || 8000;
 
-app.use(cors());
-app.use(express.static('public'));
+MongoClient.connect(process.env.DB_STRING, { useUnifiedTopology: true })
+    .then(client => {
+        console.log('connected to mongodb');
+        
+        app.use(express.static('public'));
+        app.use(cors());
 
-app.get('/', (req, res) => {
-    res.json({
-        'message': 'you have reached the base endpoint.'
-    });
-})
+        const db = client.db('project-port');
 
-app.listen(PORT, console.log(`The server is running on PORT ${PORT}, you better go catch it!`))
+        app.get('/', (req, res) => {
+            res.json({
+                'message': 'you have reached the base endpoint.'
+            });
+        })
+
+        app.listen(PORT, console.log(`The server is running on PORT ${PORT}, you better go catch it!`))
+
+    })
+    .catch(err => console.error(err))
